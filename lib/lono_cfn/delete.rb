96a2cf9ec4eb708d30bc7808869a3e6b9c18fbc9
Tung Nguyen <tongueroo@gmail.com>
2017-05-25 18:19:55 -0700

    fix delete stack exist check

--- lib/lono_cfn/delete.rb
+++ lib/lono_cfn/delete.rb
@@ -10,13 +10,13 @@ module LonoCfn
     end
 
     def run
-      message = "Deleted #{@stack_name}."
+      message = "Deleted #{@stack_name} stack."
       if @options[:noop]
         puts "NOOP #{message}"
       else
         are_you_sure?(:delete)
 
-        if stack_exist?(@stack_name)
+        if stack_exists?(@stack_name)
           cfn.delete_stack(stack_name: @stack_name)
           puts message
         else
